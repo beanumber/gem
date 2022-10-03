4d4333cfccbd8682f19c6aa2ae76d4da5e2f8918
Ben Baumer <ben.baumer@gmail.com>
2022-10-03 14:23:55 -0400

    added 2021 data

--- data-raw/foreign.R
+++ data-raw/foreign.R
@@ -13,12 +13,12 @@ sav <- dir_info("~/Data/gem/", regexp = ".sav$") %>%
 # gem2020 <- gem::read_gem_data(sav$path[6])
 # lobstr::obj_size(gem2015) / 1024^2
 
-gem <- sav %>%
-  pull(path) %>%
-#  tail(2) %>%
-  map(gem::read_gem_data) %>%
-  map(gem::clean_gem) %>%
-  set_names(sav$gem_name)
+gem <- sav |>
+  pull(path) |>
+  tail(1) |>
+  map(gem::read_gem_data) |>
+#  map(gem::clean_gem) |>
+  set_names(sav$gem_name[7])
 
 # map2(names(gem), gem, ~assign(.x, .y))
 
@@ -28,12 +28,14 @@ assign(names(gem)[3], gem[[3]])
 assign(names(gem)[4], gem[[4]])
 assign(names(gem)[5], gem[[5]])
 assign(names(gem)[6], gem[[6]])
+assign(names(gem)[7], gem[[7]])
 
 # map(sav$gem_name, usethis::use_data)
 
 usethis::use_data(
-  gem2015, gem2016, gem2017,
-  gem2018, gem2019, gem2020,
+#  gem2015, gem2016, gem2017,
+#  gem2018, gem2019, gem2020,
+  gem2021,
   overwrite = TRUE
 )
 
